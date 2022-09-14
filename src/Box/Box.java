88d8adfccd98f3f21b4b64c76aaa6eb5a4ed51df
Lenovo <altynbekuulubajsal@gmail.com>
2022-09-14 11:45:19 +0300

    1

---
+++ src/Box/Box.java
@@ -0,0 +1,9 @@
+package Box;
+
+public class Box <T>{
+
+    public static <T> T method(T o){
+              return o;
+    }
+
+}
---
+++ src/Box/Main.java
@@ -0,0 +1,7 @@
+package Box;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Box.method(new Box<Box>()));
+    }
+}
